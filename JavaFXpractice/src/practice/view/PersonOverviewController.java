7b29ba3abffdb13005fc6b209071619aba080329
Chungjung Lee <cj.chungjung.lee@gmail.com>
2017-10-10 20:40:32 +0900

    JavaFX tutorial 3 중반 완료

--- JavaFXpractice/src/practice/view/PersonOverviewController.java
+++ JavaFXpractice/src/practice/view/PersonOverviewController.java
@@ -2,7 +2,7 @@ package practice.view;
 
 import practice.MainApp;
 import practice.model.Person;
-
+import practice.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -48,8 +48,17 @@ public class PersonOverviewController {
 	@FXML
 	private void initialize() {
 		// 연락처 테이블의 두 열을 초기화한다.
-		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+		firstNameColumn.setCellValueFactory(
+				cellData -> cellData.getValue().firstNameProperty());
+		lastNameColumn.setCellValueFactory(
+				cellData -> cellData.getValue().lastNameProperty());
+		
+		// 연락처 정보를 지운다.
+		showPersonDetails(null);
+		
+		// 선택을 감지하고 그 때마다 연락처의 자세한 정보를 보여준다.
+		personTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showPersonDetails(newValue));
 	}
 	
 	/**
@@ -61,4 +70,33 @@ public class PersonOverviewController {
 		// 테이블에 observable 리스트 데이터를 추가한다.
 		personTable.setItems(mainApp.getPersonData());
 	}
+	
+	/**
+	 * 연락처의 자세한 정보를 오른쪽 tableview에 보여주기 위해 모든 텍스트 필드를 채운다
+	 * 만일 person이 null이면 모든 텍스트 필드가 지워진다. 
+	 */
+	private void showPersonDetails(Person person) {
+		if (person != null) {
+			// person 객체로 label에 정보를 채운다.
+			firstNameLabel.setText(person.getFirstName());
+			lastNameLabel.setText(person.getLastName());
+			streetLabel.setText(person.getStreet());
+			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+			cityLabel.setText(person.getCity());
+			
+			// TODO: 생일을 String으로 변환해야 한다!
+			// birthdayLabel.setText();
+			birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+		} else {
+			// person이 null이면 모든 텍스트를 지운다.
+			firstNameLabel.setText("");
+			lastNameLabel.setText("");
+			streetLabel.setText("");
+			postalCodeLabel.setText("");
+			cityLabel.setText("");
+			birthdayLabel.setText("");
+		}
+	}
+	
+	
 }
