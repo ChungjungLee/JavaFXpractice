7df1f66f7743c37d93e04f6557849e0c9c0277b5
Chungjung Lee <cj.chungjung.lee@gmail.com>
2017-10-10 16:42:52 +0900

    JavaFXtest 폴더 생성

---
+++ JavaFXpractice/src/practice/model/Person.java
@@ -0,0 +1,28 @@
+package practice.model;
+
+import java.time.LocalDate;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * 연락처 모델 클래스
+ * 
+ * @author user
+ *
+ */
+public class Person {
+	private final StringProperty firstName;
+	private final StringProperty lastName;
+	private final StringProperty street;
+	private final IntegerProperty postalCode;
+	private final StringProperty city;
+	private final ObjectProperty<LocalDate> birthday;
+	
+	
+	
+}
